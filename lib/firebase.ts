8020c09dd68c94e1b80850c386eec30f5b1c80dd
moathssnck <myneww00@gmail.com>
2025-08-10 03:22:05 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,14 +4,14 @@ import { getDatabase } from 'firebase/database';
 import { doc, getFirestore, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDgFnMZD4NHBPe6cAT1CtL1amIBqmaKzEU",
-  authDomain: "ziolm-16b34.firebaseapp.com",
-  databaseURL: "https://ziolm-16b34-default-rtdb.firebaseio.com",
-  projectId: "ziolm-16b34",
-  storageBucket: "ziolm-16b34.firebasestorage.app",
-  messagingSenderId: "669950264738",
-  appId: "1:669950264738:web:b18d8aae90b7e048c3defb",
-  measurementId: "G-XVP5TC5KKQ"
+   apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
